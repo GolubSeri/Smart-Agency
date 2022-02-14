8976ab71c78df19bc1c086cabb9d3a390566d789
GolubSeri <vlad-nikitienko@mail.ru>
2022-02-14 18:14:45 +0300

    Menu Burger

--- #Source/Js/script.js
+++ #Source/Js/script.js
@@ -49,9 +49,73 @@ new Swiper('.clients-midle', {
 	autoplay: {
 		delay: 5000,
 		stopOnLastSlide: false,
-		// Отключение после ручного переключения
 		disableOnInteraction: true,
 	},
 
 	speed: 500,
 });
+
+const iconMenu = document.querySelector('.burger__icon');
+const menuBody = document.querySelector('.burger__body');
+const mainLogo = document.querySelector('.main-header__logo');
+if(iconMenu){
+	iconMenu.addEventListener('click', function(e){
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+
+		// Если бургер закрыт
+		if (!mainLogo.classList.contains('_disable')){
+			mainLogo.classList.add('_disable');
+			setTimeout(() => {
+				mainLogo.classList.add('_display')
+			}, 200);
+		} else {
+			mainLogo.classList.remove('_display')
+			setTimeout(() => {
+				mainLogo.classList.remove('_disable')
+			}, 1);
+		}
+	});
+}
+
+// Прокрутка при клике 
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0){
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.main-header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+
+			// Закрываем меню при переходе на блок
+			iconMenu.classList.toggle('_active');
+			menuBody.classList.toggle('_active');
+			mainLogo.classList.remove('_display')
+			setTimeout(() => {
+				mainLogo.classList.remove('_disable')
+			}, 1);
+		}
+	}
+}
+
+const logoLink = document.querySelector('.logo__link');
+if(logoLink){
+	logoLink.addEventListener('click', function(e){
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		});
+		e.preventDefault();
+	});
+}
